0c1e46f700d416960361c1e0479d161a7089862d
Shuo Yang <imsure95@gmail.com>
2017-07-09 22:43:24 -0700

    update 565

--- leetcode/C++/array_nesting_565.cpp
+++ leetcode/C++/array_nesting_565.cpp
@@ -50,6 +50,56 @@ public:
   }
 };
 
+// use a vector of boolean instead of unordered_set to mark
+// the visited indices.
+
+class Solution3 {
+public:
+  int arrayNesting(vector<int>& nums) {
+    int max_size = numeric_limits<int>::min();
+    int N = nums.size();
+    int k, next_index, curr_size;
+    vector<bool> visited (N, false);
+    for (k = 0; k < N; ++k) {
+      next_index = k;
+      curr_size = 0;
+      while (visited[next_index] == false) {
+        ++curr_size;
+        visited[next_index] = true;
+        next_index = nums[next_index];
+      }
+      max_size = max(max_size, curr_size);
+    }
+
+    return max_size;
+  }
+};
+
+// put to numeric_limits<int>::max() in place for marking
+// the visited indices to achieve O(1) space.
+
+class Solution4 {
+public:
+  int arrayNesting(vector<int>& nums) {
+    int max_size = numeric_limits<int>::min();
+    int N = nums.size();
+    int k, next_index, curr_size;
+    for (k = 0; k < N; ++k) {
+      next_index = k;
+      curr_size = 0;
+      while (nums[next_index] != numeric_limits<int>::max()) {
+        ++curr_size;
+        int tmp = next_index;
+        next_index = nums[next_index];
+        nums[tmp] = numeric_limits<int>::max();
+      }
+      max_size = max(max_size, curr_size);
+    }
+
+    return max_size;
+  }
+};
+
 int main()
 {
   Solution2 sol;
