a8577a546a0f48ea2e6667fd7b2fe354fac0f9ec
Shuo Yang <imsure95@gmail.com>
2017-07-19 14:43:36 -0700

    update 532

--- leetcode/C++/kdiff_pair_in_array_532.cpp
+++ leetcode/C++/kdiff_pair_in_array_532.cpp
@@ -46,12 +46,86 @@ public:
   }
 };
 
+
+// one-pass: map + set
+// use a set to store visited nums to eliminate duplications.
+
+class Solution2 {
+public:
+  int findPairs(vector<int>& nums, int k) {
+    if (k < 0) return 0; // k cannot be negative
+
+    int len = nums.size();
+    unordered_set<int> nums_set; // store unique numbers
+    unordered_map<int, int> nums_map;
+
+    int pair_counter = 0;
+    for (int i = 0; i < len; ++i) {
+      nums_map[nums[i]] += 1;
+
+      if (nums_set.count(nums[i]) == 0 && k > 0) {
+        nums_set.emplace(nums[i]);
+        if (nums_map.find(nums[i] - k) != nums_map.end()) {
+          ++pair_counter;
+        }
+
+        if (nums_map.find(nums[i] + k) != nums_map.end()) {
+          ++pair_counter;
+        }
+      } else if (nums_set.count(nums[i]) == 0 && k == 0) {
+        if (nums_map[nums[i]] > 1) {
+          nums_set.emplace(nums[i]);
+          ++pair_counter;
+        }
+      }
+    }
+    return pair_counter;
+  }
+};
+
+
+// credit: https://discuss.leetcode.com/topic/81676/c-java-clean-code-with-explanation-set-map
+
+// one-pass: set + map
+// use a set to store smaller numbers that contribute to pairs
+// for each num in the array,
+//   - if num - k is in the map, insert num - k into the set;
+//   - if num + k is in the map, insert num into the set
+// return the size of the set
+
+// note: it handles k == 0 case directly, no need for a special case!
+
+class Solution3 {
+public:
+  int findPairs(vector<int>& nums, int k) {
+    if (k < 0) return 0; // k cannot be negative
+
+    int len = nums.size();
+    unordered_set<int> results;
+    unordered_map<int, int> nums_map;
+
+    for (int i = 0; i < len; ++i) {
+      if (nums_map.find(nums[i] - k) != nums_map.end()) {
+        results.emplace(nums[i] - k);
+      }
+
+      if (nums_map.find(nums[i] + k) != nums_map.end()) {
+        results.emplace(nums[i]);
+      }
+
+      nums_map[nums[i]] += 1;
+    }
+
+    return results.size();
+  }
+};
+
 int main()
 {
-  Solution sol;
-  vector<int> nums {3, 1, 4, 1, 5};
+  Solution3 sol;
+  // vector<int> nums {3, 1, 4, 1, 5};
   // vector<int> nums {1, 2, 3, 4, 5};
   // vector<int> nums {1, 3, 1, 5, 4};
-  // vector<int> nums {-1,-2,-3};
+  vector<int> nums {-1,-2,-3};
   cout << sol.findPairs(nums, 1) << endl;
 }
