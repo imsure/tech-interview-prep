46a2ef72b1b183e51f31e6d2f0bfc352694404af
Shuo Yang <imsure95@gmail.com>
2017-08-06 13:34:53 -0700

    update 189

--- leetcode/C++/rotate_array_189.cpp
+++ leetcode/C++/rotate_array_189.cpp
@@ -1,5 +1,7 @@
 #include "common.hpp"
 
+// with help of a stack or array
+
 // time: O(n)
 // space: O(k)
 
@@ -48,11 +50,11 @@ private:
     n = max(k, n - k);
     k > _n - k ? k = 2 * k - _n : k = k;
 
-    for (const int num : nums) {
-      cout << num << ',';
-    }
-    cout << endl;
-    cout << "n = " << n << ", k = " << k << endl;
+    // for (const int num : nums) {
+    //   cout << num << ',';
+    // }
+    // cout << endl;
+    // cout << "n = " << n << ", k = " << k << endl;
 
     _rotate(nums, n, k);
   }
@@ -77,9 +79,38 @@ public:
   }
 };
 
+// for a given array [1,2,3,4,5] and k = 2
+// - reverse the entire array: 5,4,3,2,1
+// - reverse the first k elements: 4,5
+// - reverse the last n-k elements: 1,2,3
+// then the array became: 4,5,1,2,3
+
+class Solution4 {
+public:
+  void reverse(vector<int>& nums, int start, int end) {
+    while (start < end) {
+      swap(nums[start], nums[end]);
+      --end;
+      ++start;
+    }
+  }
+
+  void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    if (n == 0) return;
+
+    k = k % n;
+    if (n <= 1 || k <= 0) return;
+
+    reverse(nums, 0, n-1);
+    reverse(nums, 0, k-1);
+    reverse(nums, k, n-1);
+  }
+};
+
 int main()
 {
-  Solution3 sol;
+  Solution4 sol;
   vector<int> nums1 {1,2,3,4,5,6};
   sol.rotate(nums1, 2);
   for (const int num : nums1) {
