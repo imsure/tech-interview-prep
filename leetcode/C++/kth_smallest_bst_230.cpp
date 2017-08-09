e73cd0de9a14cbaf14850b45de49eca527527cf6
Shuo Yang <imsure95@gmail.com>
2017-08-08 17:19:47 -0700

    update 230

--- leetcode/C++/kth_smallest_bst_230.cpp
+++ leetcode/C++/kth_smallest_bst_230.cpp
@@ -10,7 +10,7 @@ struct TreeNode {
 
 // traverse the bst in order, return the kth order node (kth smallest).
 
-// time:
+// time: O(k)
 // space:
 
 class Solution {
@@ -43,6 +43,8 @@ private:
 // credit: https://discuss.leetcode.com/topic/17573/4-lines-in-c
 // concise & elegant code!
 
+// time: O(k)
+
 class Solution2 {
 private:
   int _kthSmallest(TreeNode* root, int& k) {
@@ -58,7 +60,60 @@ public:
   }
 };
 
+// Follow up:
+// What if the BST is modified (insert/delete operations) often
+// and you need to find the kth smallest frequently?
+// How would you optimize the kthSmallest routine?
+
+// Extend the TreeNode with a count field to remember the total
+// number of nodes of the tree. Then finding the kth smallest node
+// will take just O(logN) time instead of linear time O(k).
+
+struct TreeNodeWithCount {
+  int val;
+  int count;
+  TreeNodeWithCount *left;
+  TreeNodeWithCount *right;
+  TreeNodeWithCount(int x) : val(x), count(1), left(NULL), right(NULL) {}
+};
+
+TreeNodeWithCount* buildTreeWithCount(TreeNode* root)
+{
+  if (!root) return nullptr;
+
+  TreeNodeWithCount* _root = new TreeNodeWithCount(root->val);
+  _root->left = buildTreeWithCount(root->left);
+  _root->right = buildTreeWithCount(root->right);
+  // this is wrong! Don't know why? but avoid this kind of code!
+  // _root->count += _root->left ? _root->left->count : 0 + _root->right ? _root->right->count : 0;
+  if (_root->left) _root->count += _root->left->count;
+  if (_root->right) _root->count += _root->right->count;
+  return _root;
+}
+
+class Solution3 {
+private:
+  int _kthSmallest(TreeNodeWithCount* root, int k) {
+    if (!root) return std::numeric_limits<int>::min();
+
+    if (root->left) {
+      if (k <= root->left->count) return _kthSmallest(root->left, k);
+      if (k == root->left->count + 1) return root->val;
+      return _kthSmallest(root->right, k - 1 - root->left->count);
+    } else {
+      if (k == 1) return root->val;
+      return _kthSmallest(root->right, k - 1);
+    }
+  }
+
+public:
+  int kthSmallest(TreeNode* root, int k) {
+    TreeNodeWithCount* root2 = buildTreeWithCount(root);
+    return _kthSmallest(root2, k);
+  }
+};
+
 int main()
 {
-  Solution2 sol;
+  Solution3 sol;
 }
