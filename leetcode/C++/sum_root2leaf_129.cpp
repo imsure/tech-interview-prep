94889663fac72243ce44556bfbb86a4e0a2d8501
Shuo Yang <imsure95@gmail.com>
2017-08-22 01:26:59 -0700

    add 129

---
+++ leetcode/C++/sum_root2leaf_129.cpp
@@ -0,0 +1,46 @@
+#include "common.hpp"
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// time:
+// space:
+
+class Solution {
+private:
+  void sumNumbers(TreeNode* root, int& sum, string str_val) {
+    str_val.push_back(root->val + '0');
+
+    if (!root->left && !root->right) {
+      sum += stoi(str_val);
+      return;
+    }
+
+    if (root->left) sumNumbers(root->left, sum, str_val);
+    if (root->right) sumNumbers(root->right, sum, str_val);
+  }
+
+public:
+  int sumNumbers(TreeNode* root) {
+    if (!root) return 0;
+
+    int sum = 0;
+    sumNumbers(root, sum, "");
+
+    return sum;
+  }
+};
+
+int main()
+{
+  Solution sol;
+  stringstream ss;
+  for (int i = 0; i < 10; ++i) ss << i;
+  cout << ss.str() << endl;
+  cout << stoi(ss.str()) << endl;
+}
