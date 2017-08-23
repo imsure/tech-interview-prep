fa6055765f19d28c44751e46e3fcab9a23890ed7
Shuo Yang <imsure95@gmail.com>
2017-08-23 15:48:52 -0400

    update 112

--- leetcode/C++/path_sum_112.cpp
+++ leetcode/C++/path_sum_112.cpp
@@ -33,6 +33,18 @@ public:
   }
 };
 
+
+// more concise code
+
+class Solution2 {
+public:
+  bool hasPathSum(TreeNode* root, int sum) {
+    if (!root) return false;
+    if (root->val == sum && !root->left && !root->right) return true;
+    return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+  }
+};
+
 int main()
 {
   Solution sol;
