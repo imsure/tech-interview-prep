26301e1bda95b3dc5de93d344689b9ae409791ee
Shuo Yang <imsure95@gmail.com>
2017-08-22 22:02:49 -0400

    update 103

--- leetcode/C++/btree_zigzag_traversal_103.cpp
+++ leetcode/C++/btree_zigzag_traversal_103.cpp
@@ -56,6 +56,88 @@ public:
   }
 };
 
+
+class Solution2 {
+public:
+  vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    vector<vector<int>> results;
+    if (!root) return results;
+
+    deque<TreeNode*> nodes;
+    nodes.push_back(root);
+    bool l2r = true;
+
+    int sz;
+    while (!nodes.empty()) {
+      vector<int> values;
+      sz = nodes.size();
+      while (sz--) {
+        TreeNode* node;
+        if (l2r) { // for left to right: do pop_front and push_back, left then right
+          node = nodes.front();
+          nodes.pop_front();
+          values.push_back(node->val);
+          if (node->left) nodes.push_back(node->left);
+          if (node->right) nodes.push_back(node->right);
+        } else { // for right to left: do pop_back and push_front, right then left
+          node = nodes.back();
+          nodes.pop_back();
+          values.push_back(node->val);
+          if (node->right) nodes.push_front(node->right);
+          if (node->left) nodes.push_front(node->left);
+        }
+      }
+      results.push_back(values);
+      l2r = l2r ? false : true;
+    }
+
+    return results;
+  }
+};
+
+
+// two stacks solution
+
+class Solution3 {
+public:
+  vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    vector<vector<int>> results;
+    if (!root) return results;
+
+    stack<TreeNode*> l2r_nodes;
+    stack<TreeNode*> r2l_nodes;
+    l2r_nodes.push(root);
+    bool is_left2right = true;
+
+    while (!l2r_nodes.empty() || !r2l_nodes.empty()) {
+      vector<int> values;
+      TreeNode* node;
+      if (is_left2right) {
+        while (!l2r_nodes.empty()) {
+          node = l2r_nodes.top();
+          l2r_nodes.pop();
+          values.push_back(node->val);
+          if (node->left) r2l_nodes.push(node->left);
+          if (node->right) r2l_nodes.push(node->right);
+        }
+      } else {
+        while (!r2l_nodes.empty()) {
+          node = r2l_nodes.top();
+          r2l_nodes.pop();
+          values.push_back(node->val);
+          if (node->right) l2r_nodes.push(node->right);
+          if (node->left) l2r_nodes.push(node->left);
+        }
+      }
+
+      results.push_back(values);
+      is_left2right = is_left2right ? false : true;
+    }
+
+    return results;
+  }
+};
+
 int main()
 {
   Solution sol;
