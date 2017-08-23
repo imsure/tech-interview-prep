5655bf60d66d22ca6ff291b88820ee3a17d27d7b
Shuo Yang <imsure95@gmail.com>
2017-08-23 16:04:15 -0400

    add 102

---
+++ leetcode/C++/btree_level_order_traversal_102.cpp
@@ -0,0 +1,42 @@
+#include "common.hpp"
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> results;
+    if (!root) return results;
+
+    queue<TreeNode*> cur_level;
+    cur_level.push(root);
+
+    int sz;
+    while (!cur_level.empty()) {
+      vector<int> values;
+      TreeNode* node;
+      sz = cur_level.size();
+      while (sz--) {
+        node = cur_level.front();
+        cur_level.pop();
+        values.push_back(node->val);
+        if (node->left) cur_level.push(node->left);
+        if (node->right) cur_level.push(node->right);
+      }
+      results.push_back(values);
+    }
+
+    return results;
+  }
+};
+
+int main()
+{
+  Solution sol;
+}
