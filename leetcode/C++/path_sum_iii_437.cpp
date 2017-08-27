4cfa8b4e52ffd32044b1c471ea1bd6766ddf935f
Shuo Yang <imsure95@gmail.com>
2017-08-27 12:18:22 -0700

    update 437

--- leetcode/C++/path_sum_iii_437.cpp
+++ leetcode/C++/path_sum_iii_437.cpp
@@ -64,6 +64,34 @@ public:
 };
 
 
+class Solution3 {
+private:
+  int findPath(TreeNode* root, int sum, int prev_sum,
+               unordered_map<int, int>& prefix_sums) {
+    if (!root) return 0;
+
+    int count = 0;
+    int cur_sum = prev_sum + root->val;
+    int diff = cur_sum - sum;
+    if (prefix_sums.find(diff) != prefix_sums.end()) {
+      count += prefix_sums[diff];
+    }
+    prefix_sums[cur_sum] += 1;
+
+    count += findPath(root->left, sum, cur_sum, prefix_sums);
+    count += findPath(root->right, sum, cur_sum, prefix_sums);
+    prefix_sums[cur_sum] -= 1;
+    return count;
+  }
+
+public:
+  int pathSum(TreeNode* root, int sum) {
+    unordered_map<int, int> prefix_sums;
+    prefix_sums[0] = 1;
+    return findPath(root, sum, 0, prefix_sums);
+  }
+};
+
 int main()
 {
   Solution sol;
