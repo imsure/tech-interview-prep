d5314856fa836acc3e563c269d201bed9604bf9c
Shuo Yang <imsure95@gmail.com>
2017-09-13 19:49:50 -0700

    update 101

--- leetcode/C++/symmetric_tree_101.cpp
+++ leetcode/C++/symmetric_tree_101.cpp
@@ -8,6 +8,12 @@ struct TreeNode {
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// iterative solution: check the tree level by level to see if all levels
+// are symmetric. Use a deque, push nodes (right to left) on the left subtree
+// to the front of the deque and push nodes (left to right) on the right subtree
+// to the back of the deque. Then for each pair of two nodes (one from front, one
+// from back), check if they match.
+
 class Solution {
 public:
   bool isSymmetric(TreeNode* root) {
@@ -34,6 +40,31 @@ public:
     }
 
     return true;
+  }
+};
+
+
+// recursive solution: for the two subtrees L (contains root r1 and two subtrees L1 and R1)
+// and R (contains root r2 and two subtrees L2 and R2) to be symmetric,
+// they must 1) r1 == r2 and 2) L1 and R2 are symmetric as well as R1 and L2.
+
+//    r1     |      r2
+//   /  \    |     /  \
+//  L1  R1   |    L2  R2
+
+
+class Solution2 {
+public:
+  bool isSymmetric(TreeNode* root1, TreeNode* root2) {
+    if (!root1 && !root2) return true;
+    if (!root1 || !root2) return false;
+    return (root1->val == root2->val) && isSymmetric(root1->left, root2->right)
+      && isSymmetric(root1->right, root2->left);
+  }
+
+  bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
+    return isSymmetric(root->left, root->right);
   }
 };
 
