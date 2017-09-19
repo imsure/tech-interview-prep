2f7821db6927e750f82b63598be6b14ebc6750db
Shuo Yang <imsure95@gmail.com>
2017-09-19 09:55:39 -0700

    update subsets

---
+++ misc/str_subsets.cpp
@@ -0,0 +1,44 @@
+// Print out all subsets of a given set
+
+#include "common.hpp"
+
+// time: O(2^n), this is an exhaustive search algorithm. Each call
+// makes two additional recursive calls and continues to a depth of n (size of input),
+// thus the overall algorithm is 2^n (size of "power set").
+
+// unfolding the recursion:
+// suppose input is "abc":
+//                                      "", "abc"
+//                                     /        \
+//                             "a", "bc"         "", "bc"
+//                             /      \           /     \
+//                       "ab", "c"  "a", "c"   "b", "c"  "", "c"
+//                        /    \       /    \
+//                 "abc",""  "ab","" "ac","" "a",""  "bc","", "c","", "","", "c",""
+
+class Solution {
+private:
+  void recSubsets(string sofar, string rest) {
+    if (rest.empty()) { // have exhausted all elements
+      cout << sofar << endl;
+      return;
+    }
+    // each time isolate one element from the given set
+
+    // recursively find subsets that contains the isolated element
+    recSubsets(sofar + rest[0], rest.substr(1));
+    // recursively find subsets that do not contain the isolated element
+    recSubsets(sofar, rest.substr(1));
+  }
+
+public:
+  void subsets(string str) {
+    recSubsets("", str);
+  }
+};
+
+int main()
+{
+  Solution sol;
+  sol.subsets("abcefghigklmnopq");
+}
