d0ea24f35e1870742250b71a70551f525594ce28
Shuo Yang <imsure95@gmail.com>
2017-09-19 09:46:05 -0700

    update 78

--- leetcode/C++/subsets_78.cpp
+++ leetcode/C++/subsets_78.cpp
@@ -6,8 +6,34 @@
 // - Let C = all the subsets in B augmented with A[1]
 // - return B and C
 
-// time:
-// space:
+// unfolding the recursive calls:
+// input: [1,2,3]
+//   recSubsets([1,2,3], 0, res)
+//             |
+//             |isolate [1],
+//             |res: ([], [3], [2], [2,3]) -> ([], [3], [2], [2,3], [1], [1,2], [1,3], [1,2,3])
+//             |
+//   recSubsets([1,2,3], 1, res)
+//             |
+//             |isolate [2],
+//             |res: ([], [3]) -> ([], [3], [2], [2,3])
+//             |
+//   recSubsets([1,2,3], 2, res)
+//             |
+//             |isolate [3],
+//             |res: ([]) -> ([], [3])
+//             |
+//   recSubsets([1,2,3], 3, res)
+//             |
+//             |no more element to isolate, push [] to res and return
+//             |
+// suppose input size is n, then there are total n recursive calls, for each recursive call
+// from bottom to top, we need 2^0 + 2^1 + ... + 2^(n-1) = 2^n - 1. Thus the time complexity
+// is O(2^n) since this is an exhuastive search algorithm. This also matches with the size of
+// power set which is 2^n.
+
+// time: O(2^n)
+// space: O(1)
 
 class Solution {
 private:
