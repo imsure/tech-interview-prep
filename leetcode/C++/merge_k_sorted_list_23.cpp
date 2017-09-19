cfa6ede52296ac0c67e58cae7be5e510246cc2c0
Shuo Yang <imsure95@gmail.com>
2017-09-18 18:12:13 -0700

    update 23

--- leetcode/C++/merge_k_sorted_list_23.cpp
+++ leetcode/C++/merge_k_sorted_list_23.cpp
@@ -17,7 +17,9 @@ struct ListNodeCmp {
 // Utilize the already-implmented mergeTwoLists(l1, l2) to incrementally
 // merge all the k lists into one sorted list.
 
-// time: O(kn), mergeTwoLists takes O(n) and we need to do this by k times.
+// time: O(kN), where N is the total # of nodes in all k lists.
+// mergeTwoLists takes O(n/k) time and we need to do this by k times,
+// so the total would be SUM(1<=i<=k-1)(i*N/k + N/k) = O(kN)
 // space: O(1)
 
 class Solution {
@@ -146,6 +148,68 @@ public:
     }
 
     return head;
+  }
+};
+
+
+// Divide and Conquer: merge each pair in the k lists to produce k/2 lists;
+// then merge each pair in this k/2 lists; keep doing so until all lists have
+// been merged to one list.
+
+class Solution3 {
+public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* p1 = l1;
+    ListNode* p2 = l2;
+
+    if (!p1) return p2;
+    if (!p2) return p1;
+
+    // new head points to the smallest node
+    ListNode* head;
+    if (p1->val < p2->val) {
+      head = p1;
+      p1 = p1->next;
+    } else {
+      head = p2;
+      p2 = p2->next;
+    }
+
+    ListNode* p = head;
+    while (p1 && p2) { // if we exhausted one list, stop
+      // p should point to the next smallest node across two lists
+      if (p1->val < p2->val) {
+        p->next = p1;
+        p1 = p1->next;
+      } else {
+        p->next = p2;
+        p2 = p2->next;
+      }
+      p = p->next;
+    }
+
+    // just chain the rest to the tail
+    if (p1) p->next = p1;
+    else p->next = p2;
+
+    return head;
+  }
+
+  ListNode* mergeKLists(vector<ListNode*>& lists) {
+    int n = lists.size();
+    if (!n) return nullptr;
+    if (n < 2) return lists[0];
+
+    vector<ListNode*> new_lists;
+    int i = 0, j = n - 1;
+    while (i < j) {
+      new_lists.push_back(mergeTwoLists(lists[i], lists[j]));
+      ++i; --j;
+    }
+
+    if (i == j) new_lists.push_back(lists[i]);
+
+    return mergeKLists(new_lists);
   }
 };
 
