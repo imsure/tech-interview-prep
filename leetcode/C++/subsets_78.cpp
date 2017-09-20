e2d65321de86d1cf2ae64210bb2911fd14b4c626
Shuo Yang <imsure95@gmail.com>
2017-09-19 18:14:08 -0700

    update 78

--- leetcode/C++/subsets_78.cpp
+++ leetcode/C++/subsets_78.cpp
@@ -95,6 +95,27 @@ public:
   }
 };
 
+
+// iterative solution:
+
+class Solution3 {
+public:
+  vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subs (1, vector<int>());
+    vector<int> sub;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      int n = subs.size();
+      for (int j = 0; j < n; ++j) {
+        subs.push_back(subs[j]);
+        subs.back().push_back(nums[i]);
+      }
+    }
+
+    return subs;
+  }
+};
+
 int main()
 {
   vector<int> nums {1,2,3};
