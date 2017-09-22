5b624c5f8d2e075fd06c2c44d9eb61ba18e3433e
Shuo Yang <imsure95@gmail.com>
2017-09-21 22:33:27 -0700

    add problem 57: insert intervals

---
+++ leetcode/C++/insert_intervals_57.cpp
@@ -0,0 +1,72 @@
+#include "common.hpp"
+
+// Definition for an interval.
+struct Interval {
+  int start;
+  int end;
+  Interval() : start(0), end(0) {}
+  Interval(int s, int e) : start(s), end(e) {}
+};
+
+bool IntervalCmp(Interval i1, Interval i2) {
+  return i1.start < i2.start;
+}
+
+class Solution {
+public:
+  vector<Interval> merge(vector<Interval>& intervals) {
+    vector<Interval> ans;
+    int len = intervals.size();
+    if (!len) return ans;
+
+    Interval cur = intervals[0];
+    for (int i = 1; i < len; ++i) {
+      if (cur.end >= intervals[i].start) {
+        cur.end = max(intervals[i].end, cur.end);
+      } else {
+        ans.push_back(cur);
+        cur = intervals[i];
+      }
+    }
+
+    ans.push_back(cur);
+
+    return ans;
+  }
+
+  vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+    vector<Interval> ans;
+    int len = intervals.size();
+    if (!len) { ans.push_back(newInterval); return ans;}
+
+    // binary search to find the point to insert the new interval
+    int lo = 0, hi = len - 1, mid;
+    bool need_insert = true;
+    while (lo <= hi) {
+      mid = (lo + hi) / 2;
+      if (intervals[mid].start < newInterval.start) lo = mid + 1;
+      else if (intervals[mid].start > newInterval.start) hi = mid - 1;
+      else {
+        intervals[mid].end = max(intervals[mid].end, newInterval.end);
+        need_insert = false;
+        break;
+      }
+    }
+
+    // insert new interval before lo
+    if (need_insert)
+      intervals.insert(intervals.begin() + lo, newInterval);
+
+    return merge(intervals);
+  }
+};
+
+int main()
+{
+  vector<Interval> input {Interval(1,3), Interval(6,9)};
+  Solution sol;
+  vector<Interval> output = sol.insert(input, Interval(7,10));
+  for (auto& i : output) {
+    cout << i.start << ',' << i.end << endl;
+  }
+}
