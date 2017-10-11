7e4271b672d15f0b12c996ac3fe18869a59226c0
Shuo Yang <imsure95@gmail.com>
2017-10-11 10:50:17 -0700

    update 127

--- leetcode/C++/word_ladder_127.cpp
+++ leetcode/C++/word_ladder_127.cpp
@@ -106,6 +106,59 @@ public:
 };
 
 
+// Two-end BFS solution: approach the solution from both begin and end words.
+
+class Solution3 {
+public:
+  int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    unordered_set<string> wordSet;
+    for (int i = 0; i < wordList.size(); ++i) {
+      wordSet.insert(wordList[i]);
+    }
+
+    if (!wordSet.count(endWord)) return 0;
+    if (wordSet.count(beginWord)) wordSet.erase(beginWord);
+
+    int min_dist = 1;
+    unordered_set<string> begin_set, end_set, *set1, *set2;
+    begin_set.insert(beginWord);
+    end_set.insert(endWord);
+
+    while (!begin_set.empty() && !end_set.empty()) {
+      if (begin_set.size() <= end_set.size()) { // pick smaller set as the forward set
+        set1 = &begin_set;
+        set2 = &end_set;
+      } else {
+        set1 = &end_set;
+        set2 = &begin_set;
+      }
+
+      unordered_set<string> interm_set;
+      for (auto it = set1->begin(); it != set1->end(); ++it) {
+        string word = *it;
+        wordSet.erase(word);
+        for (int i = 0; i < word.size(); ++i) {
+          char letter = word[i];
+          for (char c = 'a'; c <= 'z'; ++c) {
+            word[i] = c;
+            if (wordSet.count(word)) {
+              if (set2->count(word)) return ++min_dist;
+              interm_set.insert(word);
+            }
+          }
+          word[i] = letter;
+        }
+      }
+
+      swap(*set1, interm_set);
+      min_dist++; // increment current distance and start the next round of BFS
+    }
+
+    return 0;
+  }
+};
+
+
 int main()
 {
   vector<string> wordList1 {"hot","dot","dog","lot","log","cog"};
@@ -114,7 +167,7 @@ int main()
   // kiss -> diss -> disk -> dusk -> tusk
   vector<string> wordList4 {"miss","dusk","kiss","musk","tusk","diss","disk","sang","ties","muss"};
   vector<string> wordList5 {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
-  Solution2 sol;
+  Solution3 sol;
   cout << sol.ladderLength("hit", "cog", wordList1) << endl; // 5
   cout << sol.ladderLength("hit", "got", wordList2) << endl; // 3
   cout << sol.ladderLength("abc", "got", wordList3) << endl; // 0
