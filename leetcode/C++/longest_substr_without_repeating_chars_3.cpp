ca2c88c7cc0ef722fea300255b7905139f96b216
Shuo Yang <imsure95@gmail.com>
2017-10-13 15:16:33 -0700

    update problem 3 with comments

--- leetcode/C++/longest_substr_without_repeating_chars_3.cpp
+++ leetcode/C++/longest_substr_without_repeating_chars_3.cpp
@@ -34,13 +34,14 @@ public:
     int start = 0, end, max_len = 0;
     for (end = 0; end < s.size(); ++end) {
       if (!set.count(s[end])) {
-        set.insert(s[end]);
+        set.insert(s[end]); // remember chars that are in the current substr (without repeating ones)
         max_len = max(max_len, end - start + 1);
-      } else {
-        while (start < end) {
+      } else { // a repeating char is found
+        while (start < end) { // move start to point to the char that is right next to the repeated one
           if (s[start++] == s[end]) {
             break;
           } else {
+            // erase all the chars before the repeated one so that they can be added into the set again
             set.erase(s[start-1]);
           }
         }
