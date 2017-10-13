410c48566057ed0cd340688359386c43f023949a
Shuo Yang <imsure95@gmail.com>
2017-10-13 15:08:43 -0700

    add problem 3

---
+++ leetcode/C++/longest_substr_without_repeating_chars_3.cpp
@@ -0,0 +1,65 @@
+#include "common.hpp"
+
+// brute force: for each substr, check if all chars in it are unique
+// time: o(n^3) -> TLE
+
+class Solution {
+private:
+  bool allUnique(string s) {
+    unordered_set<char> set;
+    for (auto c : s) set.insert(c);
+    return set.size() == s.size();
+  }
+
+public:
+  int lengthOfLongestSubstring(string s) {
+    int max_len = 0;
+    for (int front = 0; front < s.size(); ++front) {
+      for (int back = s.size() - 1; back >= front; --back) {
+        if (allUnique(s.substr(front, back-front+1))) {
+          max_len = max(max_len, back-front+1);
+          break;
+        }
+      }
+    }
+
+    return max_len;
+  }
+};
+
+class Solution2 {
+public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_set<char> set;
+    int start = 0, end, max_len = 0;
+    for (end = 0; end < s.size(); ++end) {
+      if (!set.count(s[end])) {
+        set.insert(s[end]);
+        max_len = max(max_len, end - start + 1);
+      } else {
+        while (start < end) {
+          if (s[start++] == s[end]) {
+            break;
+          } else {
+            set.erase(s[start-1]);
+          }
+        }
+      }
+    }
+
+    return max_len;
+  }
+};
+
+int main()
+{
+  Solution2 sol;
+  cout << sol.lengthOfLongestSubstring("") << endl;
+  cout << sol.lengthOfLongestSubstring("abcabcbb") << endl;
+  cout << sol.lengthOfLongestSubstring("bbb") << endl;
+  cout << sol.lengthOfLongestSubstring("pwwkew") << endl;
+  cout << sol.lengthOfLongestSubstring("abcccdef") << endl;
+  cout << sol.lengthOfLongestSubstring("cabcdef") << endl;
+  cout << sol.lengthOfLongestSubstring("abcde") << endl;
+  cout << sol.lengthOfLongestSubstring("axropfaujpkfgeqohbtvqpzekndgikpkjhyzmbvxqfdyjtnsvinnznujczrmlhwvqxweyr") << endl;
+}
