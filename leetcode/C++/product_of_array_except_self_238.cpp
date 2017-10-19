c9577e0ebe797e479f7c063a17c4410fb728be74
Shuo Yang <imsure95@gmail.com>
2017-10-19 12:21:02 -0700

    update 238

--- leetcode/C++/product_of_array_except_self_238.cpp
+++ leetcode/C++/product_of_array_except_self_238.cpp
@@ -1,7 +1,7 @@
 #include "common.hpp"
 
-// time:
-// space:
+// time: O(n)
+// space: O(n)
 
 class Solution {
 public:
@@ -27,10 +27,60 @@ public:
   }
 };
 
+
+
+// product before and after can be computed within one loop instead of two.
+// time: O(n)
+// space: O(n)
+
+class Solution2 {
+public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> prod_before (n, 1); // product of nums before i (0 <= i < n)
+    vector<int> prod_after (n, 1); // product of nums after i (0 <= i < n)
+
+    for (int i = 0; i < n-1; ++i) {
+      prod_before[i+1] = prod_before[i] * nums[i];
+      prod_after[n-i-2] = prod_after[n-i-1] * nums[n-i-1];
+    }
+
+    vector<int> ans (n, 1);
+    for (int i = 0; i < n; ++i) {
+      ans[i] = prod_before[i] * prod_after[i];
+    }
+
+    return ans;
+  }
+};
+
+
+// time: O(n)
+// space: O(1)
+
+class Solution3 {
+public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+
+    vector<int> ans (n, 1);
+    int before_prod = 1, after_prod = 1;
+    for (int i = 0; i < n; ++i) {
+      ans[i] *= before_prod;
+      ans[n-i-1] *= after_prod;
+
+      before_prod *= nums[i];
+      after_prod *= nums[n-i-1];
+    }
+
+    return ans;
+  }
+};
+
 int main()
 {
   vector<int> nums {1,2,3,4};
-  Solution sol;
+  Solution2 sol;
   vector<int> res = sol.productExceptSelf(nums);
   for (auto n : res) {
     cout << n << endl;
