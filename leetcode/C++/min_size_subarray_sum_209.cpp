3cc08df2274cf1fbec4092d0c03aa7f663d8f9ce
Shuo Yang <imsure95@gmail.com>
2017-11-04 10:46:09 -0700

    update 209

--- leetcode/C++/min_size_subarray_sum_209.cpp
+++ leetcode/C++/min_size_subarray_sum_209.cpp
@@ -50,6 +50,8 @@ public:
   }
 };
 
+// time: O(nlogn)
+// space: O(n)
 
 class Solution3 {
 public:
@@ -78,9 +80,36 @@ public:
   }
 };
 
+// two pointers approach: i points to the start of each subarray
+// we are searching, j points to the end of the subarray. We sum
+// up all the elements between i and j, as soon as the current sum >= s,
+// we can update the min length and advance i by 1, also decrease
+// current sum by nums[i] and continue the search.
+
+class Solution4 {
+public:
+  int minSubArrayLen(int s, vector<int>& nums) {
+    int n = nums.size();
+    int min_len = n+1;
+
+    int i = 0, j = 0, cur_sum = 0;
+    while (i < n) {
+      if (cur_sum < s && j < n) {
+        cur_sum += nums[j++];
+      } else {
+        if (cur_sum >= s) min_len = min(min_len, j - i);
+        cur_sum -= nums[i];
+        ++i;
+      }
+    }
+
+    return min_len == n+1 ? 0 : min_len;
+  }
+};
+
 int main()
 {
-  Solution3 sol;
+  Solution4 sol;
   vector<int> nums {2,3,1,2,4,3};
   cout << sol.minSubArrayLen(7, nums) << endl;
 }
