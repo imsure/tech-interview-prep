f0041eecb4df9ab13d13977436fcfef68f5eb1e5
Shuo Yang <imsure95@gmail.com>
2017-11-23 10:35:55 -0700

    update 730

--- leetcode/C++/count_palindrome_subseq_730.cpp
+++ leetcode/C++/count_palindrome_subseq_730.cpp
@@ -1,5 +1,7 @@
 #include "common.hpp"
 
+// Brute force, memory limit exceeded and TLE
+
 class Solution {
 private:
   unordered_set<string>* _findAllPalindromes(string s, int when_to_end) {
@@ -56,14 +58,56 @@ public:
   }
 };
 
+
 class Solution2 {
 public:
   int countPalindromicSubsequences(string S) {
     int n = S.size();
+    int mod = 1000000007;
+    auto dp_ptr = new vector<vector<vector<int>>>(4, vector<vector<int>>(n, vector<int>(n)));
+    auto& dp = *dp_ptr;
+
+    for (int i = n-1; i >= 0; --i) {
+      for (int j = i; j < n; ++j) {
+        for (int k = 0; k < 4; ++k) {
+          char c = 'a' + k;
+          if (j == i) {
+            if (S[i] == c) dp[k][i][j] = 1;
+            else dp[k][i][j] = 0;
+          } else { // j > i
+            if (S[i] != c) dp[k][i][j] = dp[k][i+1][j];
+            else if (S[j] != c) dp[k][i][j] = dp[k][i][j-1];
+            else { // S[i] == S[j] == c
+              if (j == i+1) dp[k][i][j] = 2; // "aa" : {"a", "aa"}
+              else { // length is > 2
+                dp[k][i][j] = 2;
+                for (int m = 0; m < 4; ++m) { // count each one within subwindows [i+1][j-1]
+                  dp[k][i][j] += dp[m][i+1][j-1];
+                  dp[k][i][j] %= mod;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    int ans = 0;
+    for (int k = 0; k < 4; ++k) {
+      ans += dp[k][0][n-1];
+      ans %= mod;
+    }
+
+    return ans;
   }
 };
 
 int main()
 {
-  Solution sol;
+  Solution2 sol;
+  cout << sol.countPalindromicSubsequences("bccb") << endl;
+  cout << sol.countPalindromicSubsequences("aaa") << endl;
+  cout << sol.countPalindromicSubsequences("bcb") << endl;
+  cout << sol.countPalindromicSubsequences("abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba") << endl;
+  cout << sol.countPalindromicSubsequences("bdccbdbdcaacaadbdacaadabbdaababacbdbadaccccbbbccbdbddcacdbbcdaaababddadbbcabaddbbaabcdacdcddcbacdcadddbaadcbbccdcbdcaaabcbddacaccadacdcadacadccabdbadcdabaaccbcabbacdcbbbabddbcccbcbcddacaccabbabccdbbabacadbbdbcccbddaaadcdaddcccddaacbddabddabadbcbaadcaddbdccabaddcccdccbacaccccbcdccacbabddcbcbbbdbbddaabbcaddbddddaacbbccbcdbadacccbadbccdddadcbacbbdcabcbdbbaabcdcbdacabacdccdaabcbbddbdddcacdbdccbbabbaccdcbabcdacdabdddaddacdbddabcbbbdcccacbdadddbbdbcabccadbcbadddcbcddaababcabbbcbbabadaddabcbdacaadcababcbacadcdabcdbddadaadddddbaaacddaacadcdbaaacbacaabdabccbacabbabddacddcdacdcddbacbddbdcadcddabdadcdbcbbcadbcdabbbbdcdadadbbcadbcbbcccccabdaabcdacbdbcadabcdbaaaaacdaaabcdcaabddcacdcacabbaadbdbcbaaabadbdadadaccddacababbdcbaaaccaabbdaddadcabaabbdaadadcabcabbdbbdccbabbbabbcbcbdcacccbdbabdbaadccdaddaddaadadccabdcbbbccacbbaaabccaccdcabbbddbdbbacdaabbdcdacbdddbacbadccabbbdbbbdbdbcadcacddddcdbdcabccdccabaadbcbacaaababdddcbcdaaabaaabddbcabcbbcdddccdcbccbbccadccaaccccbdadcaacdcbcabacacadaabacddaadcccbcabdad") << endl;
 }
