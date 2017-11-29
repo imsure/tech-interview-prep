5b9f32c4018fbf5bf345ce260695ee08091695b3
Shuo Yang <imsure95@gmail.com>
2017-11-28 19:55:19 -0700

    update 200

--- leetcode/C++/number_of_islands_200.cpp
+++ leetcode/C++/number_of_islands_200.cpp
@@ -113,10 +113,46 @@ public:
 };
 
 
+// DFS
+
+class Solution3 {
+private:
+  void dfs(vector<vector<char>>& grid, int r, int c) {
+    int nr = grid.size();
+    int nc = grid[0].size();
+
+    grid[r][c] = '0';
+    if (r - 1 >= 0 && grid[r-1][c] == '1') dfs(grid, r - 1, c);
+    if (r + 1 < nr && grid[r+1][c] == '1') dfs(grid, r + 1, c);
+    if (c - 1 >= 0 && grid[r][c-1] == '1') dfs(grid, r, c - 1);
+    if (c + 1 < nc && grid[r][c+1] == '1') dfs(grid, r, c + 1);
+  }
+
+public:
+  int numIslands(vector<vector<char>>& grid) {
+    int nr = grid.size();
+    if (!nr) return 0;
+    int nc = grid[0].size();
+
+    int ans = 0;
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          dfs(grid, r, c);
+          ++ans;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+
+
 int main()
 {
-  Solution sol;
-  vector<vector<char>> grid {{'1','1','1','1','0'}, {'1','1','0','1','0'}, {'1','1','0','0','0'}, {'0','0','0','0','0'}};
-  // vector<vector<char>> grid {{'1','1','0','0','0'}, {'1','1','0','0','0'}, {'0','0','1','0','0'}, {'0','0','0','1','1'}};
+  Solution3 sol;
+  // vector<vector<char>> grid {{'1','1','1','1','0'}, {'1','1','0','1','0'}, {'1','1','0','0','0'}, {'0','0','0','0','0'}};
+  vector<vector<char>> grid {{'1','1','0','0','0'}, {'1','1','0','0','0'}, {'0','0','1','0','0'}, {'0','0','0','1','1'}};
   cout << sol.numIslands(grid) << endl;
 }
