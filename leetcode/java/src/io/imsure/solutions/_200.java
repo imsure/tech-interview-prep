8b86208959a644134a1e2ec94440ca8017885bef
Shuo Yang <imsure95@gmail.com>
2017-12-15 12:28:36 -0700

    update 200 and add unionfind

--- leetcode/java/src/io/imsure/solutions/_200.java
+++ leetcode/java/src/io/imsure/solutions/_200.java
@@ -2,6 +2,7 @@ package io.imsure.solutions;
 
 import java.util.Queue;
 import java.util.LinkedList;
+import io.imsure.common.UnionFind;
 
 public class _200 {
 
@@ -90,6 +91,41 @@ public class _200 {
         }
     }
 
+    // Union Find
+    public static class Solution3 {
+        public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+
+            int nr = grid.length;
+            int nc = grid[0].length;
+            int num_islands = 0;
+            UnionFind uf = new UnionFind(grid);
+            for (int r = 0; r < nr; ++r) {
+                for (int c = 0; c < nc; ++c) {
+                    if (grid[r][c] == '1') {
+                        grid[r][c] = '0';
+                        if (r - 1 >= 0 && grid[r-1][c] == '1') {
+                            uf.union(r * nc + c, (r-1) * nc + c);
+                        }
+                        if (r + 1 < nr && grid[r+1][c] == '1') {
+                            uf.union(r * nc + c, (r+1) * nc + c);
+                        }
+                        if (c - 1 >= 0 && grid[r][c-1] == '1') {
+                            uf.union(r * nc + c, r * nc + c - 1);
+                        }
+                        if (c + 1 < nc && grid[r][c+1] == '1') {
+                            uf.union(r * nc + c, r * nc + c + 1);
+                        }
+                    }
+                }
+            }
+
+            return uf.getCount();
+        }
+    }
+
     public static void main(String[] args) {
         char[][] grid1 = new char[][]{
                 {'1', '1', '0', '0', '0'},
@@ -108,6 +144,15 @@ public class _200 {
         };
         Solution2 sol2 = new Solution2();
         System.out.println(sol2.numIslands(grid2)); // 3
+
+        char[][] grid3 = new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'},
+        };
+        Solution3 sol3 = new Solution3();
+        System.out.println(sol3.numIslands(grid3)); // 3
     }
 }
 
