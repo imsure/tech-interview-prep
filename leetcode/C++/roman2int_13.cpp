2e8cc5bf8eb632e1854600ea96fcd3091fbbf0a2
Shuo Yang <imsure95@gmail.com>
2018-01-10 23:19:14 -0700

    update 13

--- leetcode/C++/roman2int_13.cpp
+++ leetcode/C++/roman2int_13.cpp
@@ -1,5 +1,7 @@
 #include "common.hpp"
 
+// Linear Scan
+
 // time:
 // space:
 
@@ -29,10 +31,110 @@ public:
   }
 };
 
+
+// State Machine
+
+enum State {
+  I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+};
+
+class Solution2 {
+public:
+  int romanToInt(string s) {
+    if (s.size() == 0) return 0;
+
+    int ans = 0;
+    unordered_map<char, State> table;
+    table['I'] = State::I;
+    table['V'] = State::V;
+    table['X'] = State::X;
+    table['L'] = State::L;
+    table['C'] = State::C;
+    table['D'] = State::D;
+    table['M'] = State::M;
+
+    State prev_state = table[s[0]];
+    State cur_state = prev_state;
+
+    for (int i = 1; i < s.size(); ++i) {
+      cur_state = table[s[i]];
+      switch(cur_state) {
+      case State::I:
+        ans += prev_state; // any states to state I would incur an increase
+        break;
+      case State::V:
+        if (prev_state >= State::V) ans += prev_state;
+        else ans -= prev_state;
+        break;
+      case State::X:
+        if (prev_state >= State::X) ans += prev_state;
+        else ans -= prev_state;
+        break;
+      case State::L:
+        if (prev_state >= State::L) ans += prev_state;
+        else ans -= prev_state;
+        break;
+      case State::C:
+        if (prev_state >= State::C) ans += prev_state;
+        else ans -= prev_state;
+        break;
+      case State::D:
+        if (prev_state >= State::D) ans += prev_state;
+        else ans -= prev_state;
+        break;
+      case State::M:
+        if (prev_state >= State::M) ans += prev_state;
+        else ans -= prev_state;
+        break;
+      default: break;
+      }
+      prev_state = cur_state;
+    }
+
+    return ans + cur_state;
+  }
+};
+
+
+// Simplifed State Machine
+
+class Solution3 {
+public:
+  int romanToInt(string s) {
+    if (s.size() == 0) return 0;
+
+    int ans = 0;
+    unordered_map<char, State> table;
+    table['I'] = State::I;
+    table['V'] = State::V;
+    table['X'] = State::X;
+    table['L'] = State::L;
+    table['C'] = State::C;
+    table['D'] = State::D;
+    table['M'] = State::M;
+
+    State prev_state = table[s[0]];
+    State cur_state = prev_state;
+
+    for (int i = 1; i < s.size(); ++i) {
+      cur_state = table[s[i]];
+      if (prev_state >= cur_state) ans += prev_state;
+      else ans -= prev_state;
+
+      prev_state = cur_state;
+    }
+
+    return ans + cur_state;
+  }
+};
+
+
 int main()
 {
-  Solution sol;
+  Solution3 sol;
   cout << sol.romanToInt("LXXXIX") << endl; // 89
   cout << sol.romanToInt("VIII") << endl; // 8
   cout << sol.romanToInt("XCIX") << endl; // 99
+  cout << sol.romanToInt("D") << endl; // 100
+  cout << sol.romanToInt("MMMDID") << endl; // 3999
 }
