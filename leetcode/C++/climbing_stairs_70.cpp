77b6629515a65d396acab19ecf5860de4ab2fb38
Shuo Yang <imsure95@gmail.com>
2018-02-13 09:23:03 -0700

    update 70

--- leetcode/C++/climbing_stairs_70.cpp
+++ leetcode/C++/climbing_stairs_70.cpp
@@ -26,9 +26,54 @@ public:
 };
 
 
+
+// Recursive approach (TLE)
+//
+// time: O(2^n)
+
+class Solution2 {
+private:
+  int _climbStairs(int i, int n) {
+    if (i > n) return 0;
+    if (i == n) return 1;
+
+    return _climbStairs(i+1, n) + _climbStairs(i+2, n);
+  }
+
+public:
+  int climbStairs(int n) {
+    return _climbStairs(0, n);
+  }
+};
+
+
+// Recursive approach with memorization
+//
+// time: O(n)
+
+class Solution3 {
+private:
+  int _climbStairs(int i, int n, unordered_map<int, int>& table) {
+    if (i > n) return 0;
+    if (i == n) return 1;
+
+    if (!table.count(i + 1)) table[i+1] = _climbStairs(i+1, n, table);
+    if (!table.count(i + 2)) table[i+2] = _climbStairs(i+2, n, table);
+
+    return table[i + 1] + table[i + 2];
+  }
+
+public:
+  int climbStairs(int n) {
+    unordered_map<int, int> table;
+    return _climbStairs(0, n, table);
+  }
+};
+
+
 int main()
 {
-  Solution sol;
+  Solution3 sol;
   cout << sol.climbStairs(2) << endl; // 2
   cout << sol.climbStairs(3) << endl; // 3
 }
