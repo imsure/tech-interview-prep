d807bf593c1ae2c4ca2df532c97f2b47c2b79763
Shuo Yang <imsure95@gmail.com>
2018-05-18 23:41:54 -0700

    update 408

--- leetcode/leetcode_py/408.py
+++ leetcode/leetcode_py/408.py
@@ -35,8 +35,42 @@ class Solution:
         # print(full_word)
         return word == full_word
 
+    def validWordAbbreviation2(self, word, abbr):
+        """
+        There are two conditions for a 'abbr' to be a valid word abbreviation:
+        - the sum of numbers and the count of chars in 'abbr' must equal to len(word)
+        - chars in 'abbr' must match with their corresponding chars in 'word'
+
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        digit_str = ''
+        n = 0
+        for i in range(0, len(abbr)):
+            if abbr[i].isdigit():
+                digit_str += abbr[i]
+                if digit_str.startswith('0'):
+                    return False
+            else:
+                n += int('0' + digit_str)
+                digit_str = ''
+                if n >= len(word) or abbr[i] != word[n]:
+                    return False
+                n += 1
+
+        n += int('0' + digit_str)
+
+        return n == len(word)
+
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.validWordAbbreviation('internationalization', 'i12iz4n'))
-    print(sol.validWordAbbreviation('apple', 'a2e'))
+    print(sol.validWordAbbreviation('internationalization', 'i12iz4n'))  # True
+    print(sol.validWordAbbreviation('apple', 'a2e'))  # False
+
+    print(sol.validWordAbbreviation2('internationalization', 'i12iz4n'))  # True
+    print(sol.validWordAbbreviation2('apple', 'a2e'))  # False
+    print(sol.validWordAbbreviation2('internationalization', 'i5a11o1'))  # True
+    print(sol.validWordAbbreviation2('word', '3e'))  # False
+    print(sol.validWordAbbreviation2('hi', '1hi'))  # False
