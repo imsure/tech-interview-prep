3a77beb464644a229c6e270283039a9f43d89d2c
Shuo Yang <imsure95@gmail.com>
2018-06-09 12:34:54 -0700

    update 609

--- leetcode/leetcode_py/609.py
+++ leetcode/leetcode_py/609.py
@@ -31,7 +31,30 @@ class Solution:
 
         return groups
 
+    def findDuplicate2(self, paths):
+        """
+        HashTable
+
+        :type paths: List[str]
+        :rtype: List[List[str]]
+        """
+        content2path = {}
+
+        for item in paths:
+            fields = item.split(sep=' ')
+            dir_path = fields[0]
+            for file in fields[1:]:
+                subfields = file.split(sep='.')
+                fname = subfields[0]
+                content = subfields[1][4:-1]
+                if content in content2path:
+                    content2path[content].append(dir_path + '/' + fname + '.txt')
+                else:
+                    content2path[content] = [dir_path + '/' + fname + '.txt']
+
+        return [*filter(lambda x: len(x) > 1, content2path.values())]
+
 
 sol = Solution()
-print(sol.findDuplicate(["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"]))
+print(sol.findDuplicate2(["root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"]))
 print(sol.findDuplicate(["root/a 1.txt(abcd) 2.txt(efsfgh)","root/c 3.txt(abdfcd)","root/c/d 4.txt(efggdfh)"]))
