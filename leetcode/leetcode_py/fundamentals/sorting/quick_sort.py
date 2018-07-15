db060727e7682ad14a803d98ad63ae2acfb4c0f9
Shuo Yang <imsure95@gmail.com>
2018-07-14 23:12:14 -0700

    add in place quick sort

--- leetcode/leetcode_py/fundamentals/sorting/quick_sort.py
+++ leetcode/leetcode_py/fundamentals/sorting/quick_sort.py
@@ -23,10 +23,72 @@ def quick_sort(array):
     return sorted_left + [pivot] + sorted_right
 
 
-print(quick_sort([]))
-print(quick_sort([4]))
-print(quick_sort([0, -5]))
-print(quick_sort([0, -5, -8]))
-print(quick_sort([-3, -2, -1, 0, 1, 2, 3]))
-print(quick_sort([5, 4, 3, 2, 1]))
-print(quick_sort([5, 8, 3, 9, 1, 0]))
+def partition(array, p, lo, hi):
+    """
+    Partition the given `array` around the value at index `p` in place and return
+    the new index of the value. The effect of this is such that all the values on
+    the left side of the index are less than or equal to the value, and all the values
+    on the right side of the index are greater than the value.
+
+    :param array:
+    :param p:
+    :return:
+    """
+    array[p], array[hi] = array[hi], array[p]
+
+    i, j = lo, hi - 1
+    while i <= j:
+        while i <= j and array[i] <= array[hi]:
+            i += 1
+        while i <= j and array[j] > array[hi]:
+            j -= 1
+
+        if i < j:
+            array[i], array[j] = array[j], array[i]
+
+    array[i], array[hi] = array[hi], array[i]
+    return i
+
+
+def quick_sort_inplace(array, lo, hi):
+    """
+
+    :param array:
+    :return:
+    """
+    if lo >= hi:
+        return
+
+    pivot_index = randint(lo, hi)
+    split_index = partition(array, pivot_index, lo, hi)
+    quick_sort_inplace(array, lo, split_index-1)
+    quick_sort_inplace(array, split_index+1, hi)
+
+
+# print(quick_sort([]))
+# print(quick_sort([4]))
+# print(quick_sort([0, -5]))
+# print(quick_sort([0, -5, -8]))
+# print(quick_sort([-3, -2, -1, 0, 1, 2, 3]))
+# print(quick_sort([5, 4, 3, 2, 1]))
+# print(quick_sort([5, 8, 3, 9, 1, 0]))
+
+array = [5, 3, 4, 6, 1]
+print(partition(array, 2, 0, len(array) - 1))
+print(array)
+
+array = [5, 3, 4, 6, 1]
+print(partition(array, 3, 0, len(array) - 1))
+print(array)
+
+array = [5, 3, 4, 6, 1]
+print(partition(array, 4, 0, len(array) - 1))
+print(array)
+
+array = [5, 8, 3, 9, 1, 0]
+quick_sort_inplace(array, 0, 5)
+print(array)
+
+array = [-3, -2, -1, 0, 1, 2, 3]
+quick_sort_inplace(array, 0, 6)
+print(array)
