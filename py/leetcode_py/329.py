3dcc487597fe8c5700c02a77bbd20765b2f50f51
Shuo Yang <imsure95@gmail.com>
2018-10-13 14:04:45 -0700

    update 329

--- py/leetcode_py/329.py
+++ py/leetcode_py/329.py
@@ -13,7 +13,7 @@ class Solution:
 
     def longestIncreasingPath(self, matrix):
         """
-        DFS
+        Brute force DFS
 
         :type matrix: List[List[int]]
         :rtype: int
@@ -33,7 +33,59 @@ class Solution:
         return ans
 
 
-sol = Solution()
+class Solution2:
+    """
+    DP + DFS, update LIP of each entry dynamically so that we can reuse the results.
+    """
+
+    def dfs(self, matrix, i, j, cur_path_len, lip_map):
+        # print('dfs on ', (i, j))
+        longest_inc_path = cur_path_len
+        n = len(matrix)
+        m = len(matrix[0])
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for dx, dy in dirs:
+            nx, ny = i + dx, j + dy
+            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] > matrix[i][j]:
+                # print(matrix[i][j], ' -> ', matrix[nx][ny])
+                if (nx, ny) in lip_map:
+                    tmp_len = cur_path_len + lip_map[(nx, ny)]
+                else:
+                    tmp_len = self.dfs(matrix, nx, ny, cur_path_len+1, lip_map)
+                longest_inc_path = max(longest_inc_path, tmp_len)
+
+        lip_map[(i, j)] = max(lip_map[(i, j)], longest_inc_path - cur_path_len + 1)
+        return longest_inc_path
+
+    def longestIncreasingPath(self, matrix):
+        """
+        DFS
+
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        import collections
+
+        ans = 0
+        n = len(matrix)
+        if n == 0:
+            return ans
+        m = len(matrix[0])
+        lip_map = collections.defaultdict(int)
+
+        for i in range(n):
+            for j in range(m):
+                cur_path_len = 1
+                longest_inc_path = self.dfs(matrix, i, j, cur_path_len, lip_map)
+                lip_map[(i, j)] = longest_inc_path
+                # print('populate entry ', (i, j))
+                ans = max(ans, longest_inc_path)
+
+        # print(lip_map)
+        return ans
+
+
+sol = Solution2()
 nums1 = [
     [9,9,4],
     [6,6,8],
@@ -46,3 +98,6 @@ nums2 = [
     [2,2,1]
 ]
 print(sol.longestIncreasingPath(nums2))  # 4
+
+nums3 = [[0,1,2,3,4,5,6,7,8,9],[19,18,17,16,15,14,13,12,11,10],[20,21,22,23,24,25,26,27,28,29],[39,38,37,36,35,34,33,32,31,30],[40,41,42,43,44,45,46,47,48,49],[59,58,57,56,55,54,53,52,51,50],[60,61,62,63,64,65,66,67,68,69],[79,78,77,76,75,74,73,72,71,70],[80,81,82,83,84,85,86,87,88,89],[99,98,97,96,95,94,93,92,91,90],[100,101,102,103,104,105,106,107,108,109],[119,118,117,116,115,114,113,112,111,110],[120,121,122,123,124,125,126,127,128,129],[139,138,137,136,135,134,133,132,131,130],[0,0,0,0,0,0,0,0,0,0]]
+print(sol.longestIncreasingPath(nums3))  # 140
